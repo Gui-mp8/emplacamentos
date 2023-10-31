2418780c82bb64a653f525f7b56f8b623510aa54
Guilherme Machado <guilherme.machado@indicium.tech>
2023-10-30 23:41:40 -0300

    feat: adding automation tests

---
+++ tests/test_endpoint.py
@@ -0,0 +1,39 @@
+import requests
+
+
+def test_endpoint_tabela_referencia():
+    response = requests.post("https://veiculos.fipe.org.br/api/veiculos/ConsultarTabelaDeReferencia")
+
+    assert response.status_code==200
+
+def test_endpoint_tabela_ano_modelo():
+    payload = {
+        'codigoTipoVeiculo': '1',
+        'codigoTabelaReferencia': '302',
+        'codigoModelo': '',
+        'codigoMarca': '',
+        'ano': '',
+        'codigoTipoCombustivel': '',
+        'anoModelo': '',
+        'tipoVeiculo': '',
+        'modeloCodigoExterno': '029157-9',
+    }
+    response = requests.post("https://veiculos.fipe.org.br/api/veiculos/ConsultarAnoModeloPeloCodigoFipe", data=payload)
+
+    assert response.status_code==200
+
+def test_endpoint_tabela_todos_parametros():
+    payload = {
+        'codigoTabelaReferencia': '302',
+        'codigoMarca': '',
+        'codigoModelo': '',
+        'codigoTipoVeiculo': '1',
+        'anoModelo': '2024',
+        'codigoTipoCombustivel': '1',
+        'tipoVeiculo': 'carro',
+        'modeloCodigoExterno': '029157-9',
+        'tipoConsulta': 'codigo'
+    }
+    response = requests.post("https://veiculos.fipe.org.br/api/veiculos/ConsultarValorComTodosParametros", data=payload)
+
+    assert response.status_code==200
---
+++ tests/test_fipe_code_data.py
@@ -0,0 +1,8 @@
+import requests
+
+def test_endpoints_response():
+    base_url = 'https://www.tabelafipebrasil.com/fipe/carros'
+    headers = {"User-Agent": 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36 OPR/103.0.0.0'}
+    response = requests.get(url=base_url, headers=headers)
+
+    assert response.status_code==200
