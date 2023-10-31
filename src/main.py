767a34da80fac2f0ede6314abe28bbb9df386743
Guilherme Machado <guilherme.machado@indicium.tech>
2023-10-31 18:20:51 -0300

    bugfix: hadling empty dicts

--- src/main.py
+++ src/main.py
@@ -16,51 +16,62 @@ from load.bigquery.table import BigQueryTable
 def main(config):
     start_time = time.time()  # Record the start time
 
-    # base_url = 'https://www.tabelafipebrasil.com/fipe/carros'
-    # headers = {"User-Agent": 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36 OPR/103.0.0.0'}
-    # fipe = FipeCode()
-    # fipe.url_base = base_url
-    # fipe.headers = headers
-    # fipe_code_list = fipe.get_soup_data()
-    # JsonFiles().writing_data(fipe_code_list, "fipe_codes")
+    base_url = 'https://www.tabelafipebrasil.com/fipe/carros'
+    headers = {"User-Agent": 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36 OPR/103.0.0.0'}
+    fipe = FipeCode()
+    fipe.url_base = base_url
+    fipe.headers = headers
+    fipe_code_list = fipe.get_soup_data()
+    JsonFiles().writing_data(fipe_code_list, "fipe_codes")
 
-    # data = ConsultarTabelaDeReferencia()
-    # data.endpoint_url = "ConsultarTabelaDeReferencia"
-    # codigo_tabela_referencia = data.get_endpoint_data()
-    # JsonFiles().writing_data(codigo_tabela_referencia, "codigo_tabela_referencia")
+    data = ConsultarTabelaDeReferencia()
+    data.endpoint_url = "ConsultarTabelaDeReferencia"
+    codigo_tabela_referencia = data.get_endpoint_data()
+    JsonFiles().writing_data(codigo_tabela_referencia, "codigo_tabela_referencia")
 
-    # ano_combustivel = []
-    # for codigo_fipe in fipe_code_list:
-    #     data = ConsultarAnoModeloPeloCodigoFipe(
-    #         codigo_tabela_referencia=codigo_tabela_referencia[0]["Codigo"],
-    #         codigo_fipe=codigo_fipe["code"],
-    #         mes_referencia=month_translation(codigo_tabela_referencia[0]["Mes"])
-    #     )
-    #     data.endpoint_url = "ConsultarAnoModeloPeloCodigoFipe"
-    #     response_data = data.get_endpoint_data()
-    #     for item in response_data:
-    #         ano_combustivel.append(item)
-    #         JsonFiles().writing_data(ano_combustivel, "ano_combustivel")
+    ano_combustivel = []
+    for codigo_fipe in fipe_code_list:
+        data = ConsultarAnoModeloPeloCodigoFipe(
+            codigo_tabela_referencia=codigo_tabela_referencia[0]["Codigo"],
+            codigo_fipe=codigo_fipe["code"],
+            mes_referencia=month_translation(codigo_tabela_referencia[0]["Mes"])
+        )
+        data.endpoint_url = "ConsultarAnoModeloPeloCodigoFipe"
+        response_data = data.get_endpoint_data()
 
-    # df = pd.read_json("/home/guilherme/Documentos/vscode/projetos/emplacamentos/data/2023-10/ano_combustivel_2023-10.json")
-    # print(df.to_string())
-    # fipe_car_data = []
+        if response_data != {}:
 
-    # for index, row in df.iterrows():
-    #     data = ConsultarValorComTodosParametros(
-    #         codigo_tabela_referencia=row["codigo_tabela_referencia"],
-    #         codigo_fipe=row["codigo_fipe"],
-    #         ano_modelo=row["ano_modelo"],
-    #         codigo_tipo_combustivel=row["codigo_tipo_combustivel"],
-    #         mes_referencia=row["mes_referencia"]
-    #     )
-    #     data.endpoint_url = "ConsultarValorComTodosParametros"
-    #     final_data = data.get_endpoint_data()
+            for item in response_data:
+                ano_combustivel.append(item)
+                JsonFiles().writing_data(ano_combustivel, "ano_combustivel")
 
-    #     fipe_car_data.append(final_data)
-    #     print(final_data)
+        else:
+            print("Empty Data, skipping this row")
 
-    #     CsvFiles().writing_data(fipe_car_data, "fipe_car_data")
+
+    df = pd.read_json("/home/guilherme/Documentos/vscode/projetos/emplacamentos/data/2023-10/ano_combustivel_2023-10.json")
+    print(df.to_string())
+    fipe_car_data = []
+
+    for index, row in df.iterrows():
+        data = ConsultarValorComTodosParametros(
+            codigo_tabela_referencia=row["codigo_tabela_referencia"],
+            codigo_fipe=row["codigo_fipe"],
+            ano_modelo=row["ano_modelo"],
+            codigo_tipo_combustivel=row["codigo_tipo_combustivel"],
+            mes_referencia=row["mes_referencia"]
+        )
+        data.endpoint_url = "ConsultarValorComTodosParametros"
+        final_data = data.get_endpoint_data()
+
+        if final_data != {}:
+            fipe_car_data.append(final_data)
+            print(final_data)
+
+            CsvFiles().writing_data(fipe_car_data, "fipe_car_data")
+        else:
+            print("Empty Data, skipping this row")
+
 
 
     BigQueryDataset(config).create_dataset()
