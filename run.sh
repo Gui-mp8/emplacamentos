37a336b9d210ad1a96d420001bd8cf1b5e385fed
Guilherme Machado <guilherme.machado@indicium.tech>
2023-10-31 00:18:17 -0300

    feat: adding python application execution automation.

--- run.sh
+++ run.sh
@@ -4,17 +4,28 @@ pytest tests/
 # Verifique o código de saída dos testes
 pytest_exit_code=$?
 
-# Inicie os containers usando docker-compose, apenas se os testes passarem
-if [ $pytest_exit_code -eq 0 ]; then
-  echo "Tests passed. Starting Application..."
-  python3 ./src/main.py
-else
-  echo "Tests failed. Application will not be started."
+if [ $pytest_exit_code -ne 0 ]; then
+  # Tests failed, so exit with a non-zero status
+  echo "Tests Failed. Application will not be started."
   exit 1
 fi
 
-# Mantenha o script em execução para manter os containers em execução
-# Você pode pressionar Ctrl+C para encerrar a execução
+echo "Test Passed!!!"
+echo "Starting Application"
+python3 ./src/main.py
+
+# Verifique o código de saída da aplicação Python
+python_exit_code=$?
+
+if [ $python_exit_code -ne 0 ]; then
+  # Application failed, so exit with a non-zero status
+  echo "Application failed."
+  exit 1
+fi
+
+echo "Application Finished"
+exit
+
 while true; do
   sleep 1
 done
